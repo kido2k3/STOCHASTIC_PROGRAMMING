40201d3925e8cc4a25218a408c18bb2cfd85b683
Nguyen Cao Tuan <nguyencaotuan2004@gmail.com>
2023-12-14 21:51:34 +0700

    feat: "Final adjustment in exercise 2"

--- exercise2/stage1.py
+++ exercise2/stage1.py
@@ -1,5 +1,6 @@
 #%%
-import main as MAIN
+import pandas
+import random
 import networkx as nx
 import matplotlib
 import matplotlib.pyplot as plt
@@ -8,6 +9,119 @@ from networkx.algorithms.flow import edmonds_karp
 #Extensions used: Juypter (search in VsCode)
 ## Type "#%%" on top of the piece of code you want to run
 #############################
+
+# Global variables
+ARRAY_TO_RANDOM = [1/2, 1/3, 1/4, 1, 1.25, 1.5, 1.75]
+NUMBER_OF_CARS = 400
+
+
+class Edge:
+    # def __init__(self, vertex_from, vertex_to, time, capacity):
+    #     self.vertex_from = vertex_from
+    #     self.vertex_to = vertex_to
+    #     self.penalty = time
+    #     self.capacity = capacity
+
+    def __init__(self, vertex_from, vertex_to):
+        self.vertex_from = vertex_from
+        self.vertex_to = vertex_to
+        self.penalty = random.randint(1, 20)
+        self.capacity = int(NUMBER_OF_CARS*random.choice(ARRAY_TO_RANDOM))
+        # objective variables
+        self.x = 0
+        self.y = 0
+
+    def cal_new_capacity(self, time, scenario, demand):
+        if scenario == 'scenario1':
+            seed = 34
+        else: 
+            seed = 22
+        var = (self.capacity + time)*seed % 400
+        while(var < demand): 
+            var = var * 2 + demand
+        return var
+
+    def cal_new_travel_time(self, time, scenario):
+        if scenario == 'scenario1':
+            seed = 34
+        else: 
+            seed = 22
+        return (self.penalty + time)*seed % 30
+
+    def __repr__(self):
+        return f'({self.vertex_from},{self.vertex_to}, p = {self.penalty}, c = {self.capacity})'
+
+
+NUMBER_OF_VARIABLES = 9
+NUMBER_OF_EDGES = 12
+TIME_LENGTH = 150
+TIME_THRESHOLD = 10
+d = [0]*NUMBER_OF_VARIABLES
+p = [0]*NUMBER_OF_EDGES
+edge = []
+scenario = ['scenario1', 'scenario2']
+
+# random.seed(10)  # Testcase $1
+# random.seed(25)  # Testcase $2
+# random.seed(30)  # Testcase $3
+# random.seed(40)  # Testcase $4
+# random.seed(52)  # Testcase $5
+# random.seed(68)  # Testcase $6
+# random.seed(73)  # Testcase $7
+# random.seed(85)  # Testcase $8
+# random.seed(91)  # Testcase $9
+random.seed(150) # Testcase $10
+
+def initial():
+    global d, edge, p
+
+    d[0] = NUMBER_OF_CARS
+    for i in range(NUMBER_OF_VARIABLES-2):
+        d[i] = 0
+    d[NUMBER_OF_VARIABLES - 1] = -NUMBER_OF_CARS
+
+    # edge.append(Edge(1, 2, 7, 50))
+    # edge.append(Edge(1, 4, 1, 100))
+
+    # edge.append(Edge(2, 3, 4, 20))
+    # edge.append(Edge(2, 5, 6, 30))
+
+    # edge.append(Edge(3, 6, 1, 50))
+
+    # edge.append(Edge(4, 5, 3, 30))
+    # edge.append(Edge(4, 7, 2, 50))
+
+    # edge.append(Edge(5, 6, 7, 30))
+    # edge.append(Edge(5, 8, 3, 120))
+
+    # edge.append(Edge(6, 9, 5, 100))
+    # edge.append(Edge(7, 8, 5, 70))
+    # edge.append(Edge(8, 9, 4, 120))
+
+    edge.append(Edge(1, 2))
+    edge.append(Edge(1, 4))
+
+    edge.append(Edge(2, 3))
+    edge.append(Edge(2, 5))
+
+    edge.append(Edge(3, 6))
+
+    edge.append(Edge(4, 5))
+    edge.append(Edge(4, 7))
+
+    edge.append(Edge(5, 6))
+    edge.append(Edge(5, 8))
+
+    edge.append(Edge(6, 9))
+    edge.append(Edge(7, 8))
+    edge.append(Edge(8, 9))
+
+    for i in range(len(edge)):
+        p[i] = edge[i].penalty
+
+    edge = sorted(edge, key=lambda x: x.vertex_from)
+
+initial()
 
 #set plot attributes
 plt_size = 20
@@ -37,7 +151,7 @@ Sample graph:
 def init():
     G = nx.DiGraph() # Create a graph with 0 node
     #Create node
-    G.add_node(1, demand=-MAIN.NUMBER_OF_CARS, color="#c53d46")
+    G.add_node(1, demand=-NUMBER_OF_CARS, color="#c53d46")
     # G.add_node(1, demand=-100, color="#c53d46")
     G.add_node(2, demand=0, color="#fff8b6")
     G.add_node(3, demand=0, color="#fff8b6")
@@ -47,11 +161,11 @@ def init():
     G.add_node(7, demand=0, color="#fff8b6")
     G.add_node(8, demand=0, color="#fff8b6")
     # G.add_node(9, demand=100, color="#b0e0e6")
-    G.add_node(9, demand=MAIN.NUMBER_OF_CARS, color="#b0e0e6")
+    G.add_node(9, demand=NUMBER_OF_CARS, color="#b0e0e6")
 
     #Create link between each node according to sample graph
-    for i in range(MAIN.NUMBER_OF_EDGES):
-        G.add_edge(MAIN.edge[i].vertex_from, MAIN.edge[i].vertex_to, weight=MAIN.edge[i].penalty, capacity=MAIN.edge[i].capacity, flow = 0)
+    for i in range(NUMBER_OF_EDGES):
+        G.add_edge(edge[i].vertex_from, edge[i].vertex_to, weight=edge[i].penalty, capacity=edge[i].capacity, flow = 0)
 
     # G.add_edge(1, 2, weight=7, capacity=50, flow=0)
     # G.add_edge(1, 4, weight=1, capacity=100, flow=0)
@@ -106,5 +220,3 @@ flowCost, flowDict = calculate(G)
 for node in flowDict:
     print(str(node) + "->" + str(flowDict[node]))
 print("Cost of flow: " + str(flowCost))
-print("---------------------------------------------")
-
--- exercise2/stage2.py
+++ exercise2/stage2.py
@@ -1,5 +1,4 @@
 #%%
-# import main as MAIN
 import stage1 as st1 
 
 '''
@@ -14,71 +13,71 @@ Sample graph:
 #Handle cost before time threshold
 #Set data for nodes
 node_data = []
-for i in range(st1.MAIN.NUMBER_OF_VARIABLES):
+for i in range(st1.NUMBER_OF_VARIABLES):
     data = {
         "Code" : i + 1,
         "Time" : 0,
         "Car_amount" : 0
     }
     if i == 0:
-        data["Car_amount"] = st1.MAIN.NUMBER_OF_CARS
+        data["Car_amount"] = st1.NUMBER_OF_CARS
     node_data.append(data)
 def node_index(code):
     index = 0
-    for i in range(st1.MAIN.NUMBER_OF_VARIABLES):
+    for i in range(st1.NUMBER_OF_VARIABLES):
         if node_data[i]["Code"] == code:
             index = i
     return index
 
 # The time before threshold
 node_after_safe = []   #List of node use for temp source
-safe_time = st1.MAIN.TIME_THRESHOLD
+safe_time = st1.TIME_THRESHOLD
 total_safe_flow = 0    #Go for plan stage 1 before time threshold
 
 #Setting dfs algorithm with completing some tasks
 def dfs_safe_path(node, time):
-    for i in range(st1.MAIN.NUMBER_OF_EDGES):
+    for i in range(st1.NUMBER_OF_EDGES):
         global total_safe_flow, node_data
-        if st1.MAIN.edge[i].vertex_from == node:
+        if st1.edge[i].vertex_from == node:
             # Amount of cars expected to move in edge according to stage 1
-            plan_flow = (st1.flowDict.get(st1.MAIN.edge[i].vertex_from)).get(st1.MAIN.edge[i].vertex_to)
+            plan_flow = (st1.flowDict.get(st1.edge[i].vertex_from)).get(st1.edge[i].vertex_to)
             if(plan_flow) != 0:
                 # Amount of cars in start node
-                current_flow = node_data[node_index(st1.MAIN.edge[i].vertex_from)]["Car_amount"]
+                current_flow = node_data[node_index(st1.edge[i].vertex_from)]["Car_amount"]
                 # Calculate the current amount of cars moving
                 if(current_flow < plan_flow):
                     plan_flow = current_flow
-                    node_data[node_index(st1.MAIN.edge[i].vertex_from)]["Car_amount"] = 0
+                    node_data[node_index(st1.edge[i].vertex_from)]["Car_amount"] = 0
                 else: 
-                    node_data[node_index(st1.MAIN.edge[i].vertex_from)]["Car_amount"] = current_flow - plan_flow
+                    node_data[node_index(st1.edge[i].vertex_from)]["Car_amount"] = current_flow - plan_flow
                 # Total cost and time after moving
-                total_safe_flow = total_safe_flow + plan_flow * st1.MAIN.edge[i].penalty
-                time = time + st1.MAIN.edge[i].penalty
+                total_safe_flow = total_safe_flow + plan_flow * st1.edge[i].penalty
+                time = time + st1.edge[i].penalty
                 # Set data to arrived node
-                node_data[node_index(st1.MAIN.edge[i].vertex_to)]["Car_amount"] = node_data[node_index(st1.MAIN.edge[i].vertex_to)]["Car_amount"] + plan_flow
-                node_data[node_index(st1.MAIN.edge[i].vertex_to)]["Time"] = time
-                print("Node to: " + str(st1.MAIN.edge[i].vertex_to))
-                print(time)
+                node_data[node_index(st1.edge[i].vertex_to)]["Car_amount"] = node_data[node_index(st1.edge[i].vertex_to)]["Car_amount"] + plan_flow
+                node_data[node_index(st1.edge[i].vertex_to)]["Time"] = time
+                    # print("Node to: " + str(st1.edge[i].vertex_to))
+                    # print(time)
                 # Calculate the situation which cars arrived to sink
-                if(st1.MAIN.edge[i].vertex_to != 9):
+                if(st1.edge[i].vertex_to != 9):
                     if time >= safe_time: 
-                        print("Threshold reach! Return to Node: " + str(st1.MAIN.edge[i].vertex_from))
-                        node_after_safe.append(node_data[node_index(st1.MAIN.edge[i].vertex_to)])
+                        # print("Threshold reach! Return to Node: " + str(st1.edge[i].vertex_from))
+                        node_after_safe.append(node_data[node_index(st1.edge[i].vertex_to)])
                     else: 
-                        dfs_safe_path(st1.MAIN.edge[i].vertex_to, time)
-                        print("Node current: " + str(st1.MAIN.edge[i].vertex_from))
+                        dfs_safe_path(st1.edge[i].vertex_to, time)
+                        # print("Node current: " + str(st1.edge[i].vertex_from))
                 else: 
-                    print("Flow reach sink!")
-                    print("Amount of flow reached sink: " + str(node_data[node_index(st1.MAIN.edge[i].vertex_to)]["Car_amount"]))
-                    node_after_safe.append(node_data[node_index(st1.MAIN.edge[i].vertex_to)])
-                time = time - st1.MAIN.edge[i].penalty     
+                    # print("Flow reach sink!")
+                    # print("Amount of flow reached sink: " + str(node_data[node_index(st1.edge[i].vertex_to)]["Car_amount"]))
+                    node_after_safe.append(node_data[node_index(st1.edge[i].vertex_to)])
+                time = time - st1.edge[i].penalty     
         else: 
             continue
     return
 
 #Finding nodes after reached threshold
 dfs_safe_path(1, 0)
-print(node_data)
+# print(node_data)
 print("Total flow before reach threshold: " + str(total_safe_flow))
 
 #Variation time to change data of edge
@@ -91,22 +90,23 @@ print("Time when data change happened: " + str(variation_time))
 #Remove duplicate arrived node after reached time threshold
 del node_after_safe
 node_after_safe = []
-for i in range(st1.MAIN.NUMBER_OF_VARIABLES - 1):
+for i in range(st1.NUMBER_OF_VARIABLES - 1):
     if node_data[i]["Car_amount"] > 0:
         node_after_safe.append(node_data[i])
+print("Source nodes after reached time threshold: ")
 print(node_after_safe)
 
 #%%
 #Handle cost after time threshold
 #Create a temp super source to include all nodes which have cars
 cars_remain = 0
-for i in range(st1.MAIN.NUMBER_OF_VARIABLES - 1):
+for i in range(st1.NUMBER_OF_VARIABLES - 1):
     if node_data[i]["Car_amount"] > 0:
         cars_remain = cars_remain + node_data[i]["Car_amount"]
 
 st1.G.add_node(0, demand=-cars_remain, color="#ffff00") #Node temp(Code: 0)
 st1.node_post[0] = (1/2, 1/2) #Node position
-for i in range(st1.MAIN.NUMBER_OF_VARIABLES - 1):
+for i in range(st1.NUMBER_OF_VARIABLES - 1):
     if node_data[i]["Car_amount"] > 0:
         st1.G.add_edge(0, node_data[i]["Code"], weight = 0, capacity = node_data[i]["Car_amount"], flow = 0)
 
@@ -125,23 +125,26 @@ egde_of_G = [st1.G.edges[1, 2], st1.G.edges[1, 4], st1.G.edges[2, 3], st1.G.edge
 min_scenario_cost = []
 
 #Calculate the min cost when cars move after threshold in each scenario
-for i in range(len(st1.MAIN.scenario)):
-    for j in range(st1.MAIN.NUMBER_OF_EDGES):
+for i in range(len(st1.scenario)):
+    for j in range(st1.NUMBER_OF_EDGES):
         #Change attribute for edge after threshold for each scenario
-        egde_of_G[j]["weight"] = st1.MAIN.edge[j].cal_new_travel_time(variation_time, st1.MAIN.scenario[i])
-        egde_of_G[j]["capacity"] = st1.MAIN.edge[j].cal_new_capacity(variation_time, st1.MAIN.scenario[i], cars_remain / 2)
-        print("Capacity of " + str(st1.MAIN.edge[j].vertex_from) + " -> " + str(st1.MAIN.edge[j].vertex_to) + ": " + str(egde_of_G[j]["capacity"]))
+        egde_of_G[j]["weight"] = st1.edge[j].cal_new_travel_time(variation_time, st1.scenario[i])
+        egde_of_G[j]["capacity"] = st1.edge[j].cal_new_capacity(variation_time, st1.scenario[i], cars_remain / 2)
+        # print("Capacity of " + str(st1.edge[j].vertex_from) + " -> " + str(st1.edge[j].vertex_to) + ": " + str(egde_of_G[j]["capacity"]))
     #Calculate the min_cost for each scenario
     cost, dict = st1.calculate(st1.G)
     st1.drawGraph(st1.G, st1.node_post)
-    print(dict)
+    print("Possible min cost path in scenario " + str(i + 1))
+    for node in dict:
+        print(str(node) + "->" + str(dict[node]))
+    # print(dict)
     print("Cost of part after time threshold in scenario " + str(i + 1) + ": " + str(cost))
     min_scenario_cost.append(cost)
 
 #Calculate the average min cost when cars move after threshold
 scenario_rate = [0.5, 0.5]
 total_after_flow = 0
-for i in range(len(st1.MAIN.scenario)):
+for i in range(len(st1.scenario)):
     total_after_flow = total_after_flow + scenario_rate[i]*min_scenario_cost[i] 
 print("Min cost value after time threshold through several scenarios: " + str(total_after_flow))
 
